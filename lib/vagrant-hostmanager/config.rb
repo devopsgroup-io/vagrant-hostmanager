0d7ecf5f4aeaca78a7e63e8aea6d0df6c08308cd
Mirosław Nagaś <mgolden@smoczus.pl>
2013-04-17 11:34:47 +0200

    add simple host aliases implementation and configuration options validation

--- lib/vagrant-hostmanager/config.rb
+++ lib/vagrant-hostmanager/config.rb
@@ -3,15 +3,44 @@ module VagrantPlugins
     class Config < Vagrant.plugin('2', :config)
       attr_accessor :auto_update
       attr_accessor :ignore_private_ip
+      attr_accessor :aliases
 
       def initialize
         @auto_update = UNSET_VALUE
         @ignore_private_ip = UNSET_VALUE
+        @aliases = Array.new 
       end
 
       def finalize!
         @auto_update = true if @auto_update == UNSET_VALUE
         @ignore_private_ip = false if @ignore_private_ip == UNSET_VALUE
+      end
+
+      def validate(machine)
+        errors = Array.new
+
+        # check if auto_update option is defined in Vagrantfile
+        # and check if is either true or false accordingly
+        if (machine.config.hostmanager.auto_update &&
+            ![TrueClass, FalseClass].include?(auto_update.class))
+          errors << "A value for hostmanager.auto_update can be true or false."
+        end
+
+        # check if ignore_private_ip option is defined in Vagrantfile
+        # and check if is either true or false accordingly
+        if (machine.config.hostmanager.ignore_private_ip &&
+            ![TrueClass, FalseClass].include?(ignore_private_ip.class))
+          errors << "A value for hostmanager.ignore_private_ip can be true or false."
+        end
+
+        # check if aliases option is defined in Vagrantfile
+        # and check if is an Array accordingly
+        if (machine.config.hostmanager.aliases &&
+            !machine.config.hostmanager.aliases.kind_of?(Array))
+          errors << "A value for hostmanager.aliases must be an Array."
+        end
+        
+        { "HostManager configuration" => errors }
       end
     end
   end
